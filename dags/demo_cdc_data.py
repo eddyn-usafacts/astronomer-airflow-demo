b79db1a79e4c58950cf644de9d9ca9bd0744aa13
Eddy Nassif <91751108+eddyn-usafacts@users.noreply.github.com>
2023-07-17 15:38:02 -0700

    getting somewhere

--- dags/demo_cdc_data.py
+++ dags/demo_cdc_data.py
@@ -45,14 +45,14 @@ def call_galactus(agent: str, payload: dict):
         raise ValueError(e.args[0])
 
 def get_most_recent_blob(container):
-    # Should be handled by https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/wasb/index.html#airflow.providers.microsoft.azure.hooks.wasb.WasbHook.get_blobs_list_recursive
-    # But it doesn't exist?
+    # Should be handled by this but get does not exist https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/wasb/index.html#airflow.providers.microsoft.azure.hooks.wasb.WasbHook.get_blobs_list_recursive
     container_client = azure._get_container_client(container)
     blobs = [blob for blob in container_client.list_blobs() if pipeline_id in blob['name']]
     if len(blobs) == 0:
         raise Exception(f'No blobs found for pipeline {pipeline_id}. Are you sure it exists?')
     # Get most recent blob name
     return max(blobs, key=lambda x: x['last_modified'])
+
 
 @aql.dataframe(task_id="wild_to_bronze")
 def wild_to_bronze_func():
@@ -66,20 +66,6 @@ def wild_to_bronze_func():
     response = call_galactus(agent, payload)
     return response.status_code
 
-@aql.dataframe(task_id="bronze_to_silver")
-def bronze_to_silver_func():
-    # # return azure.get_blobs_list_recursive('ingestions', pipelineId)  # This doesn't exist??? It's in the docs
-    # container = 'ingestions'
-    # blob_path = get_most_recent_blob(container)['name']
-    # blob_name = blob_path.split('/')[-1]
-    # # print(blob_name)
-    # # print(blob_path)
-    # blob_client = azure._get_blob_client(container, blob_path)
-    # df = pd.DataFrame(BytesIO((blob_client.download_blob().readall())), dtype=str)
-    # # print(pd.read_csv(blob_client.download_blob().readall(), dtype=str))
-    # # azure.get_file(container, blob_path, '3187bb07-94ea-4f9c-bacf-0b89506321bc/jobs/522af04d-0f33-40c3-8869-ab0161673bf4/documents/c0a89203-c93e-433d-8072-dbc1b4ce4c4e')
-    pass
-
 @aql.dataframe(task_id="bronze_to_json")
 def bronze_to_json_func():
     # This would be equivalent to an agent/service that does "bronze to parquet", but Galactus used json
@@ -88,18 +74,34 @@ def bronze_to_json_func():
     response = call_galactus(agent, payload)
     return response.status_code
 
+@aql.dataframe(task_id="json_to_silver")
+def json_to_silver_func():
+    # # return azure.get_blobs_list_recursive('ingestions', pipelineId)  # This doesn't exist??? It's in the docs
+    container = 'extractions'
+    blob_path = get_most_recent_blob(container)['name']
+    data = azure.read_file(container, blob_path)
+    df = pd.DataFrame(json.loads(data))
+
+@aql.dataframe(task_id="python_1")
+def python_1_func():
+    print(json.dumps(wasb.__dict__))
+
 @dag(
     schedule="0 0 * * *",
     start_date=pendulum.from_format("2023-07-14", "YYYY-MM-DD").in_tz("UTC"),
     catchup=False,
 )
 def demo_cdc_data():
-    bronze_to_silver = bronze_to_silver_func()
-
     wild_to_bronze = wild_to_bronze_func()
 
     bronze_to_json = bronze_to_json_func()
 
+    json_to_silver = json_to_silver_func()
+
+    python_1 = python_1_func()
+
     bronze_to_json << wild_to_bronze
+
+    json_to_silver << bronze_to_json
 
 dag_obj = demo_cdc_data()
