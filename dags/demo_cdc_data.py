df194096f984334aed0ad4345370ba5f8c068b8c
Eddy Nassif <91751108+eddyn-usafacts@users.noreply.github.com>
2023-07-17 15:00:41 -0700

    Got to extraction

--- dags/demo_cdc_data.py
+++ dags/demo_cdc_data.py
@@ -8,6 +8,8 @@ from astro import sql as aql
 import pandas as pd
 import pendulum
 
+# This is called before every cell run
+
 from airflow import DAG
 from airflow.providers.microsoft.azure.hooks import wasb
 from airflow.operators.python import PythonOperator
@@ -16,14 +18,19 @@ import pandas as pd
 import requests
 
 from datetime import datetime, timedelta
+from io import BytesIO
+import json
 import logging
 
 logger = logging.getLogger(__name__)
 
+azure = wasb.WasbHook(wasb_conn_id='test_azure_connection')  # Also has async
+
+pipeline_id = '3187bb07-94ea-4f9c-bacf-0b89506321bc'
+
 # Functions must be defined here
 def call_galactus(agent: str, payload: dict):
     # Import inside function for easy copy/paste to other demos
-    import requests
     headers = {
         'accept': 'application/json',
         'Content-Type': 'application/json',
@@ -37,24 +44,49 @@ def call_galactus(agent: str, payload: dict):
     except requests.exceptions.HTTPError as e:
         raise ValueError(e.args[0])
 
+def get_most_recent_blob(container):
+    # Should be handled by https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/wasb/index.html#airflow.providers.microsoft.azure.hooks.wasb.WasbHook.get_blobs_list_recursive
+    # But it doesn't exist?
+    container_client = azure._get_container_client(container)
+    blobs = [blob for blob in container_client.list_blobs() if pipeline_id in blob['name']]
+    if len(blobs) == 0:
+        raise Exception(f'No blobs found for pipeline {pipeline_id}. Are you sure it exists?')
+    # Get most recent blob name
+    return max(blobs, key=lambda x: x['last_modified'])
+
 @aql.dataframe(task_id="wild_to_bronze")
 def wild_to_bronze_func():
     agent = 'data_ingestion_agent_url'
     payload = {
-        'pipelineId': '3187bb07-94ea-4f9c-bacf-0b89506321bc',
+        'pipelineId': pipeline_id,
         'createBy': 'eddyn@usafacts.org',
         'codeLocation': 'airflow',
         'url': 'https://www.cdc.gov/wcms/vizdata/poxvirus/monkeypox/data/USmap_counts.csv?2032-08-18T11:30:00.000Z',
     }
     response = call_galactus(agent, payload)
-    print(response.status_code)
+    return response.status_code
 
 @aql.dataframe(task_id="bronze_to_silver")
 def bronze_to_silver_func():
-    # Can configure this to airflow env
-    wasb_url = 'wasb://DefaultEndpointsProtocol=https;AccountName=usafactsdata;AccountKey=hgIyRYwBsEeP2ylYNFGVFuNALzrHcG6HAH+yThptirFuK1mcDaezKdEKRNzBFkRTrV4WgJSJe5uCpSSJ5QJHLw==;EndpointSuffix=core.windows.net'
-    
-    wasb.wasb_default(wasb_url)  # also has async hook
+    # # return azure.get_blobs_list_recursive('ingestions', pipelineId)  # This doesn't exist??? It's in the docs
+    # container = 'ingestions'
+    # blob_path = get_most_recent_blob(container)['name']
+    # blob_name = blob_path.split('/')[-1]
+    # # print(blob_name)
+    # # print(blob_path)
+    # blob_client = azure._get_blob_client(container, blob_path)
+    # df = pd.DataFrame(BytesIO((blob_client.download_blob().readall())), dtype=str)
+    # # print(pd.read_csv(blob_client.download_blob().readall(), dtype=str))
+    # # azure.get_file(container, blob_path, '3187bb07-94ea-4f9c-bacf-0b89506321bc/jobs/522af04d-0f33-40c3-8869-ab0161673bf4/documents/c0a89203-c93e-433d-8072-dbc1b4ce4c4e')
+    pass
+
+@aql.dataframe(task_id="bronze_to_json")
+def bronze_to_json_func():
+    # This would be equivalent to an agent/service that does "bronze to parquet", but Galactus used json
+    agent = 'data_extraction_agent_csv'
+    payload = {'pipelineId': pipeline_id, 'createBy': 'eddyn@usafacts.org', 'codeLocation': 'airflow'}
+    response = call_galactus(agent, payload)
+    return response.status_code
 
 @dag(
     schedule="0 0 * * *",
@@ -62,8 +94,12 @@ def bronze_to_silver_func():
     catchup=False,
 )
 def demo_cdc_data():
+    bronze_to_silver = bronze_to_silver_func()
+
     wild_to_bronze = wild_to_bronze_func()
 
-    bronze_to_silver = bronze_to_silver_func()
+    bronze_to_json = bronze_to_json_func()
+
+    bronze_to_json << wild_to_bronze
 
 dag_obj = demo_cdc_data()
